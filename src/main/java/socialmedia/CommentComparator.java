6fc58953758d31fbd101f29b6425f96b50c5e2a2
AdarshPrusty7 <70983719+AdarshPrusty7@users.noreply.github.com>
2021-05-13 23:10:32 +0100

    Added Javadoc

--- src/main/java/socialmedia/CommentComparator.java
+++ src/main/java/socialmedia/CommentComparator.java
@@ -2,6 +2,10 @@ package socialmedia;
 
 import java.util.Comparator;
 
+/**
+* Comment Comparator Class
+* This is a single-purpose class that takes two Comments and compares the chronological order in which they were posted
+*/
 public class CommentComparator implements Comparator<Comment> {
 
     @Override
